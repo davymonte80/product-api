be0c3b442c2942df590a2e0b26d08670b7439fb9
davymonte80 <davymonte80@gmail.com>
2025-04-28 15:00:55 +0300

    add controler

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '3a97d26c0005ed664befb9147b9e198f51590605',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '3a97d26c0005ed664befb9147b9e198f51590605',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
